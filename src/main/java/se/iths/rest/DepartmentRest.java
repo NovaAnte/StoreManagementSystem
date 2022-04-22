73b9437176fcabe8a0fd9870a7d1a04712928f81
Johnny Olsen <johnnyolsen90@gmail.com>
2022-04-22 14:35:12 +0200

    Added find method for department

--- src/main/java/se/iths/rest/DepartmentRest.java
+++ src/main/java/se/iths/rest/DepartmentRest.java
@@ -24,12 +24,28 @@ public class DepartmentRest {
     @Path("")
     @POST
     public Response createDepartment(Department department) {
-        try{
+        try {
             departmentService.addDepartment(department);
         } catch (Exception e) {
             throw new WebApplicationException(Response.status(Response.Status.CONFLICT).entity(new JsonFormatter
                     (Response.Status.CONFLICT.getStatusCode(), "Could not add new department")).build());
         }
         return Response.ok(department).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getDepartmentById(@PathParam("id") Long id) {
+        notFoundError(id);
+        Department department = departmentService.findDepartmentById(id);
+        return Response.ok(department).build();
+    }
+
+    @Produces(MediaType.APPLICATION_JSON)
+    private void notFoundError(Long id) {
+
+        if (departmentService.findDepartmentById(id) == null) {
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(new JsonFormatter(404, "ID: " + id + " not found")).build());
+        }
     }
 }
