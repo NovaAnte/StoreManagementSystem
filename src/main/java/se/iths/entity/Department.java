e372778e883f1097700772d73bd8e0351f0111ea
NovaAnte <andreas_cabraja@hotmail.com>
2022-04-22 11:49:57 +0200

    Added fields to entities

--- src/main/java/se/iths/entity/Department.java
+++ src/main/java/se/iths/entity/Department.java
@@ -1,4 +1,18 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String departmentName;
+
+    // link to items, employee
 }
