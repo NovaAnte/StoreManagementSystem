8c689b8dc6b69be94643bc72a8759d6ab6022a72
Johnny Olsen <johnnyolsen90@gmail.com>
2022-04-25 11:39:30 +0200

    Added getters and setters for employee entity.

--- src/main/java/se/iths/entity/Employee.java
+++ src/main/java/se/iths/entity/Employee.java
@@ -11,20 +11,53 @@ public class Employee {
     @GeneratedValue (strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
     @Size(min = 2)
     private String name;
 
-    @NotEmpty
     @Size(min = 2)
     private String role;
 
-    @NotEmpty
     @Column(unique = true)
     private String email;
 
     @NotEmpty
     private double Salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(double salary) {
+        Salary = salary;
+    }
 
     // link to department
 }
