297c0f66f4071950cdc404e06cd856b610c609c7
Ava Wulkan <ava.wulkan@gmail.com>
2022-04-25 12:51:39 +0200

    Added fields, getters and setters and constructor to Item

--- src/main/java/se/iths/entity/Item.java
+++ src/main/java/se/iths/entity/Item.java
@@ -1,9 +1,6 @@
 package se.iths.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
@@ -16,5 +13,60 @@ public class Item {
     @NotEmpty
     private String name;
 
-    // link to department, shoppingcart, customer
+    @NotEmpty
+    private double price;
+
+    @ManyToOne
+    private Department department;
+
+    @ManyToOne
+    private ShoppingCart shoppingCart;
+
+    public Item() {
+    }
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
 }
